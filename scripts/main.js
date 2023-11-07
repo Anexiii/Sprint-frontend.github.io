bcc9c73df3b597f88e2b87b38c7b83e0bb7169c6
Anexi <joaquindel82@gmail.com>
2023-11-07 13:54:46 -0300

    agregacion de extras

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,53 @@
+// Función para manejar el envío del formulario
+function manejarFormularioSubmit(event) {
+  event.preventDefault();
+
+  const form = document.querySelector('form');
+  const nombre = document.querySelector('#nombre');
+  const correo = document.querySelector('#correo');
+  const asunto = document.querySelector('#asunto');
+  const mensaje = document.querySelector('#mensaje');
+  const mensajeEnviado = document.getElementById('mensaje-enviado');
+
+  if (nombre.value === '' || correo.value === '' || asunto.value === '' || mensaje.value === '') {
+    mensajeEnviado.textContent = 'Por favor, complete todos los campos.';
+    mensajeEnviado.style.color = 'red';
+  } else {
+    mensajeEnviado.textContent = 'Formulario enviado exitosamente.';
+    mensajeEnviado.style.color = 'yellow';
+    form.reset();
+  }
+
+  mensajeEnviado.style.display = 'block';
+}
+
+// Función para manejar la adición de comentarios
+function manejarAgregarComentarioClick() {
+  const nuevoComentario = document.getElementById('nuevoComentario');
+  const listaComentarios = document.getElementById('comentarios');
+  const comentario = nuevoComentario.value;
+
+  if (comentario !== '') {
+    const nuevoElemento = document.createElement('li');
+    nuevoElemento.textContent = comentario;
+    listaComentarios.appendChild(nuevoElemento);
+    nuevoComentario.value = ''; // Limpia el campo de entrada
+  }
+}
+
+// Asignar las funciones a los eventos correspondientes
 document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector('form');
-    const nombre = document.querySelector('#nombre');
-    const correo = document.querySelector('#correo');
-    const asunto = document.querySelector('#asunto');
-    const mensaje = document.querySelector('#mensaje');
-    const mensajeEnviado = document.getElementById('mensaje-enviado');
-    const nuevoComentario = document.getElementById('nuevoComentario');
-    const listaComentarios = document.getElementById('comentarios');
-  
-    form.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
-      if (nombre.value === '' || correo.value === '' || asunto.value === '' || mensaje.value === '') {
-        mensajeEnviado.textContent = 'Por favor, complete todos los campos.';
-        mensajeEnviado.style.color = 'red';
-      } else {
-        mensajeEnviado.textContent = 'Formulario enviado exitosamente.';
-        mensajeEnviado.style.color = 'yellow';
-        form.reset();
-      }
-  
-      mensajeEnviado.style.display = 'block';
-    });
-  
-    document.getElementById('agregarComentario').addEventListener('click', function() {
-      const comentario = nuevoComentario.value;
-      if (comentario !== '') {
-        const nuevoElemento = document.createElement('li');
-        nuevoElemento.textContent = comentario;
-        listaComentarios.appendChild(nuevoElemento);
-        nuevoComentario.value = ''; // Limpia el campo de entrada
-      }
-    });
+  const form = document.querySelector('form');
+  const agregarComentarioButton = document.getElementById('agregarComentario');
+
+  form.addEventListener('submit', manejarFormularioSubmit);
+  agregarComentarioButton.addEventListener('click', manejarAgregarComentarioClick);
+
+  // Evento para mostrar todos los comentarios
+  const mostrarComentariosButton = document.getElementById('mostrarComentarios');
+  const listaComentarios = document.getElementById('comentarios');
+
+  mostrarComentariosButton.addEventListener('click', function() {
+    listaComentarios.style.display = 'block';
   });
-  
+});
